d2ee8f60f04929b7348479e15b7af14aa29968c1
Gabriel Meireles <gmeireles@2mundos.net>
2021-01-21 19:14:58 -0300

    Updating the port

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --bind 0.0.0.0 app:app --timeout 90 --log-file -
+web: gunicorn --bind 0.0.0.0:${PORT} app:app --timeout 90 --log-file -
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from helpers.data import Data
+import os
 
 app = Flask(__name__)
 
@@ -22,4 +23,5 @@ def get_player_stats(id):
         })
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
